b9035e23c939467e430bfa1fe105b73d78404e8a
Zach Callanan <caveofmonsters@gmail.com>
2020-11-10 11:53:52 +0100

    completed redo

---
+++ app/models/flat.rb
@@ -0,0 +1,3 @@
+class Flat < ApplicationRecord
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London 1',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London 2',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1512916194211-3f2b7f5f7de3'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London 3',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London 4',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1521583344519-5e3716672372'
+)
+
+puts 'Done!'
